62ae5c33065a613018f921b0f529d56137cc20f2
Nzo <73233840+Dehan667@users.noreply.github.com>
2022-05-20 16:38:01 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// Les bouttons 
+const addButton = document.querySelector('.addItemInput');
+const supButton = document.getElementById("sub");
+const toutsupButton = document.getElementById("multiply");
+
+const ajouter= document.querySelector('.Ajouter');
+const supprimer = document.querySelector('Supprimer');
+const toutsupprimer= document.querySelector('Tout Supprimer');
+
+
+// Ajouter
+
+function ajouter() {
+    
+    shoppingList.push(item);
+    displayList();
+
+// Supprimer
+
+const removeItemButton = document.querySelector('button.removeItemButton');
